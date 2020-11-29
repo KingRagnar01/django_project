018540bd8d6b7e323e0bb919b97f954c78519ad0
Rahul-del875 <61898276+Rahul-del875@users.noreply.github.com>
2020-11-29 09:32:37 +0530

    Update serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -62,3 +62,29 @@ class UserProfileSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+    class AuthSerializer(serializers.Serializer):
+    email =serializers.CharField()
+    password= serializers.CharField(
+                style={'input_type' : 'password'},
+                trim_whitespace=False
+                )
+
+    def validate(self,attrs):
+        email=attrs.get('email')
+        password = attrs.get('password')
+
+        user=authenticate(
+                request=self.context.get('request'),
+                username=email,
+                password=password
+                )
+
+        if not user:
+            msg = _("Unable to authenticate with given credentials provided")
+
+            raise serializers.ValidationError(msg,code='authentication')
+
+        attrs['user']=user
+        return attrs
+
